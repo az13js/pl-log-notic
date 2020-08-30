f81a13d5d46ca2180dfb4215e2c29c60cfad9b6f
az13js <1654602334@qq.com>
2020-08-30 23:43:08 +0800

    view中使用非阻塞的job发现存在问题

--- pladmin/pltplconf/views.py
+++ pladmin/pltplconf/views.py
@@ -2,9 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 
+# 定时任务操作
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.conf import settings
+from django_apscheduler.jobstores import DjangoJobStore
+from apscheduler.triggers.cron import CronTrigger
+
+def my_job():
+    fw = open("/tmp/xdebug.log", "w+")
+    fw.write("RUN\n")
+    fw.close()
+
 def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'pltplconf/index.html')
 
 def jsontest(request):
-    return JsonResponse({'foo': 'bar'})
+    #scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+    scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+    scheduler.add_job(
+        my_job,
+        trigger=CronTrigger(second="*"),
+        id="my_job",
+        max_instances=1,
+        replace_existing=True,
+    )
+    scheduler.start()
+    #scheduler.shutdown()
+    return JsonResponse({'foo': 'bar', 'jobs': str(scheduler.get_jobs())})
