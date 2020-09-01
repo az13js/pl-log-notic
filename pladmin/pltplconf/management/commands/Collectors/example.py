f1776441cd5bdef9c974364fe7bf606c69242827
az13js <1654602334@qq.com>
2020-09-01 15:15:26 +0800

    对接搜索引擎测试，能成功查询到结果

---
+++ pladmin/pltplconf/management/commands/Collectors/example.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+
+"""示例用
+在Linux系统中命令行方式使用代理服务，方式是设置环境变量：
+http_poxy="http://user:password@www.example.cn:1000"
+https_poxy="http://user:password@www.example.cn:1000"
+"""
+from elasticsearch import Elasticsearch
+import json
+
+class example():
+    def find(self):
+        """find方法返回字符串，字符串内容是查询的消息文本"""
+        es = Elasticsearch(
+            [{"host": "127.0.0.1", "port": 80, "url_prefix": "elasticsearch"}],
+            headers={"kbn-version":"4.5.4","Host":"xxxxxxxx.cn","User-Agent":"Mozilla/5.0 Gecko/20100101 Firefox/68.0"},
+            timeout=10,
+            http_compress=False
+        )
+
+        query_data='''
+{"index":["project_app-2020.09.01"],"ignore_unavailable":true}
+{"size":2,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"query":{"filtered":{"query":{"query_string":{"query":"environment:\\"production\\" AND message:\\"goods_tree\\"","analyze_wildcard":true}},"filter":{"bool":{"must":[{"range":{"@timestamp":{"gte":1598941792528,"lte":1598942692528,"format":"epoch_millis"}}}]}}}},"fields":["message"],"fielddata_fields":["@timestamp"]}
+'''
+        results = es.msearch(query_data)
+        return json.dumps(results)
--- pladmin/pltplconf/management/commands/testel.py
+++ pladmin/pltplconf/management/commands/testel.py
@@ -2,24 +2,12 @@
 
 from django.core.management.base import BaseCommand, CommandError
 from elasticsearch import Elasticsearch
+from pltplconf.management.commands.Collectors.example import example
 
 class Command(BaseCommand):
     help = "测试搜索"
 
     def handle(self, *args, **options):
-        es = Elasticsearch(
-            [
-                {"host": "127.0.0.1", "port": 80, "url_prefix": "elasticsearch"},
-            ],
-            headers={"kbn-version":"4.5.4","Host":"example.com.cn","User-Agent":"Mozilla/5.0 Gecko/20100101 Firefox/68.0"},
-            timeout=5,
-            http_compress=True
-        )
-        if False == es.ping():
-            self.stdout.write(self.style.ERROR("ping失败"))
-            return False
-        print(es.info())
-        print(es.cluster.health())
-        print(es.cluster.stats())
+        self.stdout.write(self.style.SUCCESS(example().find()))
 
 
