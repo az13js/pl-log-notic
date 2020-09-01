423ac9eee2f7e593e8122b4111ed25744853033d
az13js <1654602334@qq.com>
2020-09-01 13:01:05 +0800

    连接Elasticsearch成功

--- pladmin/pltplconf/management/commands/testel.py
+++ pladmin/pltplconf/management/commands/testel.py
@@ -7,12 +7,19 @@ class Command(BaseCommand):
     help = "测试搜索"
 
     def handle(self, *args, **options):
-        es = Elasticsearch(["http://127.0.0.1:8088/elasticsearch"],headers={"kbn-version":"4.5.4","Host":"example.com.cn","User-Agent":"Mozilla/5.0 Gecko/20100101 Firefox/68.0"},timeout=20)
-        #if False == es.ping():
-        #    self.stdout.write(self.style.ERROR("ping失败"))
-            #return False
-        #print(es.info())
+        es = Elasticsearch(
+            [
+                {"host": "127.0.0.1", "port": 80, "url_prefix": "elasticsearch"},
+            ],
+            headers={"kbn-version":"4.5.4","Host":"example.com.cn","User-Agent":"Mozilla/5.0 Gecko/20100101 Firefox/68.0"},
+            timeout=5,
+            http_compress=True
+        )
+        if False == es.ping():
+            self.stdout.write(self.style.ERROR("ping失败"))
+            return False
+        print(es.info())
         print(es.cluster.health())
-        #print(es.cluster.stats())
+        print(es.cluster.stats())
 
 
