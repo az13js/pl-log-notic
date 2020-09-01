11d586d2ca2f955b31c341ddcc66f4d75133447d
az13js <1654602334@qq.com>
2020-09-01 17:20:01 +0800

    后续替换的项

--- pladmin/pltplconf/management/commands/Collectors/example.py
+++ pladmin/pltplconf/management/commands/Collectors/example.py
@@ -11,6 +11,7 @@ import json
 class example():
     def find(self):
         """find方法返回字符串，字符串内容是查询的消息文本"""
+        # TODO 配置项： host，headers，放到数据库层面，json存储，转换，给应用用户灵活配置
         es = Elasticsearch(
             [{"host": "127.0.0.1", "port": 80, "url_prefix": "elasticsearch"}],
             headers={"kbn-version":"4.5.4","Host":"xxxxxxxx.cn","User-Agent":"Mozilla/5.0 Gecko/20100101 Firefox/68.0"},
@@ -18,6 +19,7 @@ class example():
             http_compress=False
         )
 
+        # TODO 配置项： query_data，放到数据库层面，json存储，转换，给应用用户灵活配置
         query_data='''
 {"index":["project_app-2020.09.01"],"ignore_unavailable":true}
 {"size":2,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"query":{"filtered":{"query":{"query_string":{"query":"environment:\\"production\\" AND message:\\"goods_tree\\"","analyze_wildcard":true}},"filter":{"bool":{"must":[{"range":{"@timestamp":{"gte":1598941792528,"lte":1598942692528,"format":"epoch_millis"}}}]}}}},"fields":["message"],"fielddata_fields":["@timestamp"]}
