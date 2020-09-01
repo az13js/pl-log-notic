9c280b9c6120f49e5057941b5f0a6ddf8e0c0f23
az13js <1654602334@qq.com>
2020-09-01 12:21:25 +0800

    Connection to ***.com.cn timed out

--- pladmin/pltplconf/management/commands/testel.py
+++ pladmin/pltplconf/management/commands/testel.py
@@ -7,12 +7,12 @@ class Command(BaseCommand):
     help = "测试搜索"
 
     def handle(self, *args, **options):
-        es = Elasticsearch(["127.0.0.1"], http_compress=True)
-        if False == es.ping():
-            self.stdout.write(self.style.ERROR("ping失败"))
-            return False
-        print(es.info())
+        es = Elasticsearch(["http://127.0.0.1:8088/elasticsearch"],headers={"kbn-version":"4.5.4","Host":"example.com.cn","User-Agent":"Mozilla/5.0 Gecko/20100101 Firefox/68.0"},timeout=20)
+        #if False == es.ping():
+        #    self.stdout.write(self.style.ERROR("ping失败"))
+            #return False
+        #print(es.info())
         print(es.cluster.health())
-        print(es.cluster.stats())
+        #print(es.cluster.stats())
 
 
