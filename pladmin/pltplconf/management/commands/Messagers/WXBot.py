8e5254a702594c11430f8e55a471a0d2a5eddc97
az13js <1654602334@qq.com>
2020-10-12 09:42:41 +0800

    推送的任务限制字符，因为微信有字符限制

--- pladmin/pltplconf/management/commands/Messagers/WXBot.py
+++ pladmin/pltplconf/management/commands/Messagers/WXBot.py
@@ -13,12 +13,17 @@ logger = logging.getLogger(__name__)
 class WXBot():
     def send(self, job, message):
         taskSetting = job.task_setting
+        # 没达到推送频率限制的可以推送：
         if PlPushLog.objects.filter(job_pri_key=job.id, push_time__gte=timezone.now() - timedelta(hours=1)).count() < taskSetting.max_per_hour:
-            logger.debug("WXBot 向企业微信群发送消息")
+            logger.debug("WXBot 向企业微信群发送消息，消息内容是")
+            pushMessage = message
+            if len(pushMessage) > 1024:
+                pushMessage = pushMessage[0:1024]
+            logger.debug(pushMessage)
             data = {
                 "msgtype": "text",
                 "text": {
-                    "content": message
+                    "content": pushMessage
                 }
             }
             logger.debug(message)
